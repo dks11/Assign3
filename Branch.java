823ec158d626b6dc348067a49a99fcd066582084
dks11 <47439162+dks11@users.noreply.github.com>
2019-02-16 14:58:17 -0400

    Update Branch.java
    Added main code to it

--- Branch.java
+++ Branch.java
@@ -1,1 +1,44 @@
-
+public class Branch
+{
+  private String branchId;
+  private String branchLocation;
+  private double[] quarterProfit;
+  private int quarterCount;
+  
+  public Branch(String Id, String location)
+  {
+       branchId = Id;
+       branchLocation = location;
+  }
+  
+  public void calculateQuarterlyProfit(double expense, double revenue) throws InvalidQuaterException
+  {
+    if ( (quarterCount < 0) || (quarterCount > 3) )
+    {
+      throw new InvalidQuaterException("Invalid Quarter");
+    }
+    else
+    double[quarterCount] = expense - revenue;
+    quarterCount++;
+  }
+  
+  public double getQuarterlyProfit(int quarter)
+  {
+    return quarterLocation[quarter];
+  }
+  
+  public double getAnnualProfit()
+  {
+    double total;
+    
+    for(int i = 0; i < 4; i++)
+    {
+      total += double[i];
+    }
+  }
+  
+  public String toString()
+  {
+   return branchId + "\t" + branchLocation + "\t" + quarterProfit[0] + "\t" + quarterProfit[1 + "\t" + quarterProfit[2]
+          + "\t" + quarterProfit[3];
+  }
